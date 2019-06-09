4102171cc878c5e8ceba1de9c45904b52ed8cd39
Sutyn <50803632+Sutyn@users.noreply.github.com>
2019-06-09 01:07:50 -0300

    Garen :Kappa:

---
+++ SutynGaren/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace LoadEncrypt
+{
+    using System;
+    using System.Reflection;
+    using System.Security.Permissions;
+
+    using EnsoulSharp.SDK;
+
+    using LoadEncrypt.Properties;
+
+    // 1. Add your DLL on Resources
+    // 2. Resource DLL build Action: Embedded Resource
+    // 2. install script on EnsoulSharp.Loader
+    // 3. test it work fine <3
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnGameLoad;
+        }
+
+        [PermissionSet(SecurityAction.Assert, Unrestricted = true)]
+        private static void OnGameLoad()
+        {
+            try
+            {
+                var a = Assembly.Load(Resources.Script);
+                var myType = a.GetType("Script.Example");// namespace + class name
+                var methon = myType.GetMethod("Init", BindingFlags.Public | BindingFlags.Static); // methon
+
+                if (methon != null)
+                {
+                    methon.Invoke(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
